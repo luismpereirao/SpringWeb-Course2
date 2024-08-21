b10b9f38f1418ae8ac65b3e566375a865ee2f27d
luismpereirao <luismpereiraovejero@gmail.com>
2024-08-22 00:08:37 +0200

    Creation of API components

---
+++ springboot-di/src/main/java/com/luisma/springboot/di/app/springboot_di/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.luisma.springboot.di.app.springboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.luisma.springboot.di.app.springboot_di.models.Product;
+import com.luisma.springboot.di.app.springboot_di.repositories.ProductRepository;
+
+public class ProductService {
+
+    private ProductRepository repository = new ProductRepository();
+
+    public List<Product> findAll() {
+
+        return repository.findAll().stream().map(p -> {
+            Double priceImp = p.getPrice() * 1.21d;
+            Product newProd = new Product(p.getId(), p.getName(), priceImp.longValue());
+            return newProd;
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(Long id) {
+        return repository.findById(id);
+    }
+}
